9901e7436cad9077b94fb27e659f78ea423281ed
Martiphe <90713693+Martiphe@users.noreply.github.com>
2021-12-18 20:02:28 +0300

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -1,12 +1,13 @@
-x = int(input('Введите число: '))
-
 import random
 
-def prime(n):
-    if 2**(n-1) % n == 1:
-        return True
-    else:
-        return False
+def ferma(n):
+    if n > 2:
+        for i in range(25):
+            a = random.randint(2, n - 1)
+            if pow(a, n-1, n) != 1:
+                return False
+    return True
 
-print(prime(x))
-
+print("Введите число: ")
+a = int(input())
+print(ferma(a))
