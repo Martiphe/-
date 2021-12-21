02fff99e0169023af5613011430b06901f408ce2
Martiphe <90713693+Martiphe@users.noreply.github.com>
2021-12-21 22:32:45 +0300

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -1,33 +1,19 @@
 import random
+import math
 
-def is_prime(n):
+def is_ferma(n):
+    if n < 2:
+        return False
     if n > 3:
         for i in range(25):
             if pow(random.randint(2, n - 1), n-1, n) != 1:
                 return False
-        return rabin_miller(n)
-    else:
-        return True
-
-
-def rabin_miller(n):
-    u = n - 1
-    t = 0
-    while not u % 2:
-        u //= 2
-        t += 1
-    d = []
-    d.append(3**u % n)
-    if d[0] == 1 or d[0] == n - 1:
-        return True
-    for i in range(1, t):
-        d.append(3**(2**i * u) % n)
-        if d[i] != n - 1:
-            return False
+        for i in range(2, int(math.sqrt(n))+1):
+            if n % i == 0:
+                return False
     return True
-
 
 
 print("Введите число: ")
 a = int(input())
-print(is_prime(a), '\n')
+print(is_ferma(a), '\n')
