f9a36945e96fd5ca5b5ee18fb351f639795cff9a
Martiphe <90713693+Martiphe@users.noreply.github.com>
2022-03-05 00:03:26 +0300

    Update pic.py

--- modules&packets/pic.py
+++ modules&packets/pic.py
@@ -4,5 +4,5 @@ import prime
 a = int(input("Введите число: "))
 
 if prime.is_prime(a) is True:
-    os.startfile(r'C:\Users\artem\PycharmProjects\pythonProject1\photocreator (1).jpg')
-else: os.startfile(r'C:\Users\artem\PycharmProjects\pythonProject1\photocreator.jpg')
+    os.startfile(r'C:\Users\artem\PycharmProjects\pythonProject1\p1.jpg')
+else: os.startfile(r'C:\Users\artem\PycharmProjects\pythonProject1\p2.jpg')
