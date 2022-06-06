191ce1fa7a226fcb1a0fbdd3dc5052aa6b12310b
Martiphe <90713693+Martiphe@users.noreply.github.com>
2022-06-06 20:41:49 +0300

    Update app_database.py

--- app_database.py
+++ app_database.py
@@ -8,25 +8,32 @@ cur.execute("""CREATE TABLE IF NOT EXISTS students (
 )""")
 con.commit()
 
-student_name = input('Фамилия и имя студента: ')
-student_grade = input('Зачёт: ')
-a = student_grade
-cur.execute("SELECT * FROM students WHERE name=?", (student_name,))
+quest = input('Хотите посмотреть таблицу, не изменяя данные(ответьте "да" или "нет"): ')
 
-if cur.fetchone() is None:
-    cur.execute("INSERT INTO students VALUES(?, ?)",(student_name,student_grade))
-    con.commit()
-    cur.execute("SELECT * FROM students")
-    print('Успеваемость')
+if quest == 'да':
     for value in cur.execute("SELECT * FROM students"):
         print(value)
+
 else:
-    for value in cur.execute("SELECT * FROM students WHERE name=?", (student_name,)):
-        cur.execute("UPDATE students SET grade = ? where name =?", (student_grade,student_name))
+    student_name = input('Фамилия и имя студента: ')
+    student_grade = input('Зачёт: ')
+
+    cur.execute("SELECT * FROM students WHERE name=?", (student_name,))
+
+    if cur.fetchone() is None:
+        cur.execute("INSERT INTO students VALUES(?, ?)", (student_name, student_grade))
         con.commit()
+        cur.execute("SELECT * FROM students")
+        print('Успеваемость')
         for value in cur.execute("SELECT * FROM students"):
             print(value)
+    else:
+        for value in cur.execute("SELECT * FROM students WHERE name=?", (student_name,)):
+            cur.execute("UPDATE students SET grade = ? where name =?", (student_grade, student_name))
+            con.commit()
+            for value in cur.execute("SELECT * FROM students"):
+                print(value)
 
-con.close())
 
 
+con.close()
